aba342351b553381473243f3dcfbd575e30f6aaf
Lucas Martin <driftpvp@gmail.com>
2023-07-05 14:58:39 -0500

    albums added to array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,21 @@
 console.log('***** Music Collection *****')
+let collection = [];
+
+function addToCollection(titleIn, artistIn, yearPublishedIn) {
+    let albumInfo = {
+        title: titleIn,
+        artist: artistIn,
+        yearPublished: yearPublishedIn,
+    }
+    collection.push (albumInfo);
+    return albumInfo;
+}
+
+addToCollection( 'Ten' , 'Pearl Jam' , 1991 );
+addToCollection( 'Nevermind' , 'Nirvana' , 1991 );
+addToCollection( 'Superunknown' , 'Soundgarden' , 1994 );
+addToCollection( 'Blood Sugar Sex Magik' , 'Red Hot Chili Peppers' , 1991 );
+addToCollection( 'Dirt' , 'Alice in Chains' , 1992 );
+addToCollection( 'Siamese Dream' , 'Smashing Pumpkins' , 1993 );
+
+console.table(collection);
